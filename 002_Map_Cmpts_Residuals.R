1db66faa45ea508c9f7f41c3d14be57b4742e2bf
xc308 <xc308@exeter.ac.uk>
2021-10-27 20:07:08 +0100

    map Cmpts residuals

---
+++ 002_Map_Cmpts_Residuals.R
@@ -0,0 +1,11 @@
+#==========================#
+# Map the Cmpts' Residuals
+#==========================#
+
+# if residuals exhibit spatial patterns, 
+# then justify further spatial modelling
+
+# 
+
+
+
