1829a393ce7cb44cd347421d1e11ef22406e6b2a
xc308 <xc308@exeter.ac.uk>
2021-10-27 16:10:06 +0100

    df_Cmpts_Mn_Res_long_log

--- 001_Recap_24May.R
+++ 001_Recap_24May.R
@@ -82,62 +82,65 @@ str(fitted_BC_Residual_log)
 
 
 
-#======================#
-# df_Cmpts_Res_long_log (spot a mistake)
-#======================#
+#=========================#
+# df_Cmpts_Mn_Res_long_log
+#=========================#
 
-load("/Users/xchen/OneDrive - University of Exeter/XC_PhD/Data/Processed/XC_WORK/RData/df_Cmpts_Res_long_log.RData")
-
-str(df_Cmpts_Res_long_log)
-# 'data.frame':	821520 obs. of  7 variables:
-  #$ Lon     : num  -42.8 -42 -39.8 -38.2 -37.5 ...
-  #$ Lat     : num  83.2 83.2 83.2 83.2 83.2 ...
-  #$ Year    : num  2016 2016 2016 2016 2016 ...
-  #$ ID      : int  1 2 3 4 5 6 7 8 9 10 ...
-  #$ Cmpts   : chr  "BC" "BC" "BC" "BC" ...
-  #$ Values  : num  -4.84 -4.98 -5.01 -5.01 -4.97 ...
-  #$ Residual: num  0.307 0.305 0.301 0.299 0.298 ...
-
-# 821520 / 6 = 136920 = 27384 * 5yr
-
-head(df_Cmpts_Res_long_log)
-# Residual: after global detrending i, "~", "Lon + Lat + I(Lat ^ 2)"
-
-tail(df_Cmpts_Res_long_log)
+# spot mistake in df_Cmpts_Res_long_log, model fitted data is wrong
+# so refit below
 
 
+#------------------------------#
+# df_all_log to df_all_long_log
+#------------------------------#
 
-#--------------------#
-# Detrending using lm
-#--------------------#
+head(df_all_log)
+df_all_long_log <- df_all_log %>% 
+  gather(key = Cmpts, value = Values_log, -Lon, -Lat, -Year, -ID)
+
+str(df_all_long_log)
+# 'data.frame':	821520 obs. of  6 variables:
+    #$ Lon       : num  -42.8 -42 -39.8 -38.2 -37.5 ...
+    #$ Lat       : num  83.2 83.2 83.2 83.2 83.2 ...
+    #$ Year      : num  2016 2016 2016 2016 2016 ...
+    #$ ID        : int  1 2 3 4 5 6 7 8 9 10 ...
+    #$ Cmpts     : chr  "BC" "BC" "BC" "BC" ...
+    #$ Values_log: num  -4.84 -4.98 -5.01 -5.01 -4.97 ...
 
 
+# 821520 = 136920 * 6 = (27384 * 5) * 6
 
+
+#-------------------------------------------------#
+# Fit 6 models in one go to get fitted value & resd
+#-------------------------------------------------#
+
+lm_fit_Cmpts <- list()
 form_Compts <- list()
+df_Cmpts <- list()
 for (i in unique(df_all_long_log$Cmpts)) {
-  form_Compts <- filter(df_Cmpts_Res_long_log, Cmpts == i) %>%
-    as.formula(paste0(i, "~", "Lon + Lat + I(Lat^2)")) %>%
-  lm_fit_cmpts <- lm(formula = form_Compts[[i]], data = df_all_log)
-  
-}
-
-
-
-
-
-
-df_Cmpts <-list()
-for(i in unique(df_all_long_log$Cmpts)) {
-  
   df_Cmpts[[i]] <- filter(df_all_long_log, Cmpts == i)
-  form_cmpts[[i]] <- as.formula(paste0(i, "~", "Lon + Lat + I(Lat ^ 2)"))
-  lm_fit_cmpts[[i]] <- lm(form_cmpts[[i]], data = df_all_log)
-  df_Cmpts[[i]]$Residual <- lm_fit_cmpts[[i]]$residuals
+  form_Compts[[i]] <- as.formula(paste0(i, "~", "Lon + Lat + I(Lat^2)"))
+  lm_fit_Cmpts[[i]] <- lm(formula = form_Compts[[i]], data = df_all_log)
   
+  df_Cmpts[[i]]$Fitted_Val_log <- lm_fit_Cmpts[[i]]$fitted.values
+  df_Cmpts[[i]]$Residuals_log <- lm_fit_Cmpts[[i]]$residuals
 }
 
-df_Cmpts_Res_long_log <- do.call("rbind", df_Cmpts) # 821520 * 7 = 136920 * 6 components * 7 var
+str(df_Cmpts)
 
+df_Cmpts_Mn_Res_long_log <- do.call("rbind", df_Cmpts)
+str(df_Cmpts_Mn_Res_long_log) # 821520 = 136920 * 6 Cmpts = (27384 * 5 Yr) * 6
+
+# cf:
+str(df_Cmpts_Res_long_log)
+
+#all(df_Cmpts$BC$Residuals_log == filter(df_Cmpts_Res_long_log, Cmpts == "BC"))
+#[1] FALSE
+
+
+save(df_Cmpts_Mn_Res_long_log, 
+     file = "/Users/xchen/OneDrive - University of Exeter/XC_PhD/Data/Processed/XC_WORK/RData/df_Cmpts_Mn_Res_long_log.rda")
 
 
 
