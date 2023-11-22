2daff26dbcf46dcb9bb1b4e9d3d3598ac4de742d
xc308 <xc308@exeter.ac.uk>
2023-11-22 22:25:40 +0000

    Fn_Tst_fymm_pd

---
+++ Fn_Tst_sym_pd.R
@@ -0,0 +1,34 @@
+#=============
+# 22 Nov. 2023
+#=============
+
+# Function:
+  # This is a function to test the symmetry and p.d. of a matrix
+  # If a matrix is symmetry, 
+    # keep testing if all eigen values are positive; 
+
+  # If a matrix is not symmetry, 
+    # it must not be p.d.
+
+
+Tst_sym_pd <- function(tst.M){
+  
+  if (isSymmetric(tst.M) == TRUE){
+    print("Symmetric: Yes")
+    
+    if (all(eigen(tst.M, symmetric = T, only.values = T)$val > 0)){
+      print("p.d.: Yes")
+    } else {print("p.d.: No")}
+    
+  }else{print("Symmetric: No")}
+}
+
+
+#======
+# Test
+#======
+
+#Tst_sym_pd(SIGMA_inv_5_pert)
+#Tst_sym_pd(SG_SG_inv_5_TST2$SIGMA_inv)
+
+
