645f8c39dab19b696d1297cb1c6cbadd98e98c56
xc308 <xc308@exeter.ac.uk>
2023-11-24 13:03:21 +0000

    wave_v6

--- Fn_Waves.R
+++ Fn_Waves.R
@@ -1,7 +1,21 @@
-#==========
-# Wave_v4
-#==========
-# This is a function of Triangular wave_v4
+#===============================
+# Wave Functions (23 Nov. 2023)
+#===============================
+
+# wave_v5: a further modified tri-wave 
+  # modifications:
+        # 1. smaller compact support range: /h - dlt/ < /dlt/ (not <= 2/dlt/)
+        # 2. faster decay of correlation: 2(/h - dlt/ / /dlt/)^2 (not 1/2 (.)^2)
+  # advantages:
+        # smaller compact support and faster decay of correlation within the spprt
+        # result in a contract matrix for p.d.
+  # Arg:
+        # h: displacement
+        # delta: width and skewness
+        # A: amplitude
+  
+
+# wave_v4: a function of Triangular wave_v4
     # modified on top of wave_v3
 
 # Modification advananges: 
@@ -20,6 +34,29 @@
         # 1. an intrinsic anisotropic stationary 
         # 2. more pertinent to real cross-correlation
 
+
+#---------
+# wave_v6
+#---------
+wave_v6 <- function(h, delta, A = 1) {
+  A * (1- (1/2)*(abs(h - delta) / abs(delta))^2)*(abs(h - delta) < abs(delta))
+}
+
+
+
+#--------
+# wave_v5
+#--------
+wave_v5 <- function(h, delta, A = 1) {
+  
+  A * (1 - 2 * (abs(h - delta) / abs(delta))^2) * (abs(h-delta) < abs(delta)) 
+}
+
+
+
+#---------
+# Wave_v4
+#--------
 wave_v4 <- function(h, delta, A = 1) {
   A * (1- (1/2)*(abs(h - delta) / abs(delta))^2)*(abs(h - delta) < 2*abs(delta))
 }
