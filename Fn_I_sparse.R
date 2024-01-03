3bf64e8275ce7b3937e131088534c6cd1418084a
xc308 <xc308@exeter.ac.uk>
2024-01-03 21:31:15 +0000

    sparse identity

--- Fn_I_sparse.R
+++ Fn_I_sparse.R
@@ -7,17 +7,10 @@
       # size: the dimension of the Identity matrix
       # value: the value on the main diagonal
 
-I_spar <- function(size, value) {
+I_sparse <- function(size, value) {
   
   require("Matrix")
   sparseMatrix(i = 1:size, j = 1:size, x = value)
   
 }
-
-#=======
-# Test
-#=======
-I_spar(size = 5, value = 1e-3)
-
-
 
