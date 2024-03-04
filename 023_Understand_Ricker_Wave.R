443b72c4b2565d00b3f46689ecad0d44ab1d5b46
xc308 <xc308@exeter.ac.uk>
2024-03-04 17:24:15 +0000

    try more parameter combination

--- 023_Understand_Ricker_Wave.R
+++ 023_Understand_Ricker_Wave.R
@@ -20,14 +20,20 @@ t_values <- seq(-1, 1, by = 0.01)
 t_values <- seq(-1, 1, by = 0.1)
 t_values <- seq(-10, 10, by = 0.1)
 t_values <- seq(-100, 100, by = 1)
+t_values <- seq(-100, 100, by = 10)
 t_values <- seq(-10000, 10000, by = 80)
 
 # Peak frequency
+f0 <- 100
+f0 <- 50
 f0 <- 25  # Adjust the peak frequency as desired
 f0 <- 5
 f0 <- 10
 f0 <- 1
+f0 <- 0.5
+f0 <- 0.2
 f0 <- 0.1
+f0 <- 0.05
 f0 <- 0.01
 f0 <- 0.001
 f0 <- 0.0001
@@ -72,6 +78,7 @@ ricker_wavelet_dlt <- function(t, f0, A, dlt) {
 t_values <- seq(-1, 1, by = 0.1)
 t_values <- seq(-1, 1, by = 0.01)
 f0 <- 1
+
 dlt <- 0.5
 dlt <- 0
 R_w_dlt <- ricker_wavelet_dlt(t = t_values, f0 = f0,  A = 1, dlt = dlt)
@@ -123,7 +130,32 @@ dev.off()
 
 
 
+#--------------------
+# decay f0 not square
+#--------------------
+ricker_wavelet_dlt_2 <- function(t, f0, A, dlt) {
+  term1 <- (1 - 2 * pi^2 * f0 * (abs(t - dlt))^2)
+  term2 <- exp(-pi^2 * f0 * (abs(t - dlt))^2)
+  result <- A * term1 * term2
+  return(result)
+}
 
+t_values <- seq(-1, 1, by = 0.01)
+t_values <- seq(-100, 100, by = 1)
+f0 <- 0.01
+
+R_w_value <- ricker_wavelet_dlt_2(t = t_values, f0 = f0, A = 1, dlt = 0)
+
+plot(t_values, R_w_value, type = "l")
+
+
+#----------
+# Discover
+#----------
+# if turn f0^2 to f0, then when grid becomes large
+  # and the decay f0 needs to lower to echo the low
+  # oscillation, f0^2 will speed up the decrease of oscialtion
+  # the curve will be broader 
 
 
 
