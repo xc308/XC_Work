ab83a3d650d9b1dbc6ef2988e38eab1d534dc977
xc308 <xc308@exeter.ac.uk>
2024-03-07 21:27:29 +0000

    Fn_cokrig

---
+++ Fn_cokrig.R
@@ -0,0 +1,126 @@
+#=============
+# 7 Mar. 2024
+#=============
+
+# Aim:
+  # cokrig function for C.V. 
+
+
+# Arg:
+  # optm_pars_vec: a vector of optimized parameters
+  # all_pars_lst: list of all pars reflecting the graph structure
+  # p: # of variate fields
+  # data_str: hierachy data reflecting DAG among variate fields
+  # h: matrix of displacement
+  # H_adj: Adjancency matrix reflecting spatial neighbourhood strucuture for a univariate field
+  # phi: the spatial decay parameter, set to be the 1/max(eigen(H_adj)$val)
+  # b: the b function, can be "Wendland" (all positive) and "Tri-Wave" (both neg/postive)
+  # obs_indx: indexing observations in the df 
+  # df: containing spatial locations, and corresponding observations for different variates
+
+
+
+
+cokrig <- function(optm_pars_vec = optm_pars, all_pars_lst, p, data_str, 
+                   phi, H_adj, h, b = "Wendland", obs_indx, df){
+  
+  source("Fn_I_sparse.R")
+  source("Fn_vec2mat.R")
+  source("Fn_TST10c_SpNReg_Thres_SG_SGInv_b_choice.R") # for TST
+  
+  optm_pars_lst <- vec_2_mat(vector = optm_pars_vec, all_pars_lst = all_pars_lst)
+  
+  
+  optm_SG_SG_inv <- TST10c_SpNReg_Thres_SG_SGInv(p = p, data = data_str, 
+                                                 A_mat = optm_pars_lst[[1]],
+                                                 dlt_mat = optm_pars_lst[[2]],
+                                                 sig2_mat = optm_pars_lst[[3]],
+                                                 phi = phi, H_adj = H_adj,
+                                                 h = H, b = b, 
+                                                 reg_ini = 1e-9, thres_ini = 1e-3)
+  
+  
+  optm_SG_Y <- optm_SG_SG_inv$SIGMA
+  # str(optm_SG_Y) # num [1:600, 1:600]
+  optm_SG_Y_obs <- optm_SG_Y[obs_indx, obs_indx]
+  #str(optm_SG_Y_obs) # num [1:550, 1:550]
+  
+  # construct SG_Ng_obs
+  tau2_optm <- optm_pars_vec[-(1:length(Vals))]
+  tau2_optm_mat <- diag(tau2_optm)
+  
+  n1 <- nrow(df)
+  I_mat <- I_sparse(size = n1, value = 1)
+  SG_Ng <- kronecker(tau2_optm_mat, I_mat)
+  SG_Ng_obs <- SG_Ng[obs_indx, obs_indx]
+  
+  
+  # construct SG_Z_obs
+  SG_Z_obs <- optm_SG_Y_obs + SG_Ng_obs
+  
+  
+  # construct SG_Z_obs_inv
+  SG_Z_obs_chol <- chol(SG_Z_obs)
+  SG_Z_obs_inv <- chol2inv(SG_Z_obs_chol)
+  
+  # all joint observations Z
+  Z <- c(df$Z1, df$Z2, df$Z3, df$Z4, df$Z5, df$Z6)
+  
+  # cokring
+  all_true_mu <- optm_SG_Y[, obs_indx] %*% SG_Z_obs_inv %*% Z[obs_indx]
+  #str(all_true_mu) # Formal class 'dgeMatrix', but can still sub-selection
+  all_true_var <- diag(optm_SG_Y - optm_SG_Y[, obs_indx] %*% SG_Z_obs_inv %*% optm_SG_Y[obs_indx, ])
+  
+  
+  # save true mu
+  sub_vec <- length(all_true_mu) %/% p 
+  each_true_mu_lst <- lapply(1:p, function(i) all_true_mu[((i-1)*sub_vec + 1) : (i*sub_vec)])
+  
+  for(i in 1:p){
+    df[paste0("true_mu", i)] <- each_true_mu_lst[[i]]
+  }
+  
+  # save true var
+  sub_vec <- length(all_true_var) %/% p
+  each_var_lst <- lapply(1:p, function(i) all_true_var[((i-1)*sub_vec + 1):(i*sub_vec)])
+  
+  for (i in 1:p){
+    df[paste0("true_var", i)] <- each_var_lst[[i]]
+  }
+  
+  
+  # return df to preserve the above changes
+  return(df)
+}
+
+
+#=============
+# Test cokring
+#==============
+# try using optimzed pars in 047c_WL
+
+#source("Fn_Tst_Fit_Obs_indx.R")
+#Tst_Fit_Obs_lst <- Fn_Tst_Fit_Obs_indx(df = df_WL, num_folds = 4)
+#Obs_indx <- Tst_Fit_Obs_lst$Obs_indx
+#obs_indx <- Obs_indx[[1]]
+
+#p = 6
+
+#df_cokrig <- cokrig(optm_pars_vec = optm_pars_WL_converged, 
+ #                   all_pars_lst = all_pars_lst_CAR_6,
+  #                  p = p, data_str = hierarchy_data6, 
+   #                 phi = phi, H_adj = H_adj, h = H,
+    #                b = "Wendland", obs_indx = obs_indx, df = df_WL)
+                    
+# r 6 
+#SG_inv 
+#[1] "Symmetric: Yes"
+#[1] "p.d.: Yes"
+#Final reg_num: 1e-09 
+#ini thres: 0.001     
+
+#df_cokrig$true_var1
+
+
+
+
