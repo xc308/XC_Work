72a1bf0253de1321260cfc33782cebd75294127c
xc308 <xc308@exeter.ac.uk>
2024-03-12 11:11:15 +0000

    kappa difficult to converge

--- 049b_ID_Inf_Fig12_TST9d_TW_&_WL.R
+++ 049b_ID_Inf_Fig12_TST9d_TW_&_WL.R
@@ -14,7 +14,7 @@ source("049_ID_Inf_Fig12_TST9d_neg_logL_Matern.R")
 # Settings
 #==========
 
-ini <- c(1, 0.1, 1, 2) # A, dlt, sig2, kappa
+ini <- c(0.5, 0.5, 1, 2) # A, dlt, sig2, kappa
 Vals <- c()
 for (i in 1:length(all_pars_lst)){
   value <- rep(ini[i], sum(is.na(all_pars_lst[[i]])))
@@ -46,15 +46,15 @@ optm_pars_Matern_TW <- optim(par = c(Vals, rep(1, p)), # ini guess
                              fn = neg_logL_Matern,
                              p = p, data_str = hierarchy_data6, 
                              all_pars_lst = all_pars_lst_Matern_6, df = df_TW, 
-                             fit_indx = fit_indx,
+                             fit_indx = fit_indx, b = "Tri-Wave",
                              method = "L-BFGS-B",
                              lower = lower_bound,
-                             control = list(trace = 1, 
-                                            maxit = 2,
-                                            pgtol = 1e-5))
+                             control = list(trace = 0, 
+                                            maxit = 200,
+                                            pgtol = 1e-4))
 
 
-
+# 1st run
 # final  value 1717.052764 
 #stopped after 3 iterations
 optm_pars_Matern_TW$message
@@ -62,6 +62,88 @@ optm_pars_Matern_TW$message
 optm_pars_Matern_TW$counts
 # function gradient 
 # 12       12 
+
+# 2nd run 
+#final  value 1563.972442 
+#stopped after 85 iterations
+optm_pars_Matern_TW$message
+# [1] "ERROR: ABNORMAL_TERMINATION_IN_LNSRCH"
+# initial value
+
+optm_pars_Matern_TW$counts
+# function gradient 
+# 256      256 
+
+optm_pars_Matern_TW$par
+# [1] 0.48628902 0.54297915
+#[3] 0.55838092 0.53285438
+#[5] 0.49051338 0.50782285
+#[7] 0.50001489 0.49953224
+#[9] 0.49947753 0.08658082
+#[11] 0.42727072 2.22067814
+#[13] 1.05183670 0.42125199
+#[15] 0.47794978 0.19150125
+#[17] 0.56442973 0.65038259
+#[19] 1.63223177 1.15320121
+#[21] 2.07270914 1.55084153
+#[23] 1.40549876 0.67715591
+#[25] 2.57817159 1.36318412
+#[27] 2.96140610 2.47055638
+#[29] 1.55639986 1.17298566
+#[31] 1.24305039 1.42695151
+#[33] 1.63231830 1.44185847
+#[35] 1.40587505 1.58684220
+
+optm_pars_Matern_TW$convergence
+# [1] 52
+
+
+# 3rd run:
+  # use the returned pars as 3rd run initial value
+  # meanwhile change pgtol = 1e-4
+optm_pars_Matern_TW$message
+#[1] "ERROR: ABNORMAL_TERMINATION_IN_LNSRCH"
+optm_pars_Matern_TW$convergence 
+#[1] 52
+
+optm_pars_Matern_TW$counts
+#function gradient 
+# 103      103 
+
+optm_pars_Matern_TW$value
+# [1] 1648.297
+
+optm_pars_Matern_TW$par
+
+# [1] 0.4829771 0.4803281 0.5261407
+#[4] 0.4673359 0.5177298 0.5000071
+#[7] 0.4995936 0.5015581 0.4936938
+#[10] 2.2549959 0.3976075 2.6997030
+#[13] 0.5816490 0.6544571 0.4532069
+#[16] 0.5492003 0.2490240 0.7504331
+#[19] 1.3748299 1.1990764 1.2761730
+#[22] 0.9253619 1.3559191 0.3015382
+#[25] 1.3712188 1.2371381 1.2213733
+#[28] 1.2601855 1.1369781 1.0350282
+#[31] 1.3310886 1.2168976 1.6480171
+#[34] 1.7890516 1.7061369 1.8597839
+
+
+#============
+# Conclusion
+#============
+# For non-cross MRF, Matern + DAG
+  # it's very difficulty to get the converged optimization results
+  # due to the kappa in Matern is highly unlikely to converge
+
+
+
+## from this point of view, cross-MRF evaude the kappa
+  # and has a set parameters that are much easier to fit.
+
+
+
+
 
 # During a line search, the objective function may be evaluated multiple times 
 # to find the step size that minimizes the objective function along the search direction
