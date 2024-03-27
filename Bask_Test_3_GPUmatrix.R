00bb3c1605526442d172f9d525714f049ca232e7
xc308 <xc308@exeter.ac.uk>
2024-03-27 11:51:43 +0000

    Test 3

---
+++ Bask_Test_3_GPUmatrix.R
@@ -0,0 +1,67 @@
+#=============
+# 27 Mar. 2024
+#=============
+
+# Aim:
+  # Test if GPUmatrix package can work on Bask
+  
+#remotes::install_github("mlverse/torch", build = FALSE)
+
+
+#install.packages("bit64")
+#library(bit64)
+install.packages("torch")
+library(torch)
+
+
+install.packages("GPUmatrix")
+library(GPUmatrix)
+Yes
+# Not torch or tensorflow installed
+
+install.packages("dplyr")
+library(dplyr)
+
+
+#====================
+# installTorch check
+#====================
+# check if torch has installed correctly
+
+installTorch()
+# [1] TRUE
+
+
+
+#============
+# gpu.matrix 
+#============
+
+# create and store a matrix in the GPU
+
+# Returns a GPUmatrix object that can be either "gpu.matrix.tensorflow" 
+  # gpu.matrix.torch".
+
+# If the gpu.matrix-class object is not sparse it will show on the console the matrix as it is. If the
+#gpu.matrix is sparse, it will return to the console the position where there are number different from
+#zero.
+
+gpu_a <- gpu.matrix(1:9,nrow=3,ncol=3)
+class(gpu_a)
+# [1] "gpu.matrix.torch"
+# attr(,"package")
+gpu_a@gm$is_cuda
+
+
+
+#gpu_b <- gpu.matrix(1:9,nrow=3,ncol=3, type = "tensorflow")
+#class(gpu_b)
+# r went aborted
+
+gpu_a@gm$is_cuda
+
+a <- gpu.matrix(1:9,nrow=3,ncol=3, device="cpu")
+class(a)
+a@gm$is_cuda
+#[1] FALSE
+
