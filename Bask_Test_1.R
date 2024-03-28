51292d589886b8c173e4b3b27d18fdd290c55524
xc308 <xc308@exeter.ac.uk>
2024-03-28 13:30:35 +0000

    2.rds for scp try

--- Bask_Test_1.R
+++ Bask_Test_1.R
@@ -101,10 +101,9 @@ str(SG_SGinv_CAR_6_2D_TW$SIGMA_inv)
 
 
 #saveRDS(SG_SGinv_CAR_6_2D_TW, file = "SG_SGinv_CAR_6_2D_TW.rds")
+saveRDS(SG_SGinv_CAR_6_2D_TW, file = "SG_SGinv_CAR_6_2D_TW_2.rds")
 
-#saveRDS(SG_SGinv_CAR_6_2D_TW$SG, file = "SG_SGinv_CAR_6_2D_TW.rds")
 
 
-#str(SG_SGinv_CAR_6_2D_TW$SG_inv)
 
 
