564fd206c2070968efb4e181342d85f9b5470070
xc308 <xc308@exeter.ac.uk>
2024-03-29 19:59:05 +0000

    add install_torch

--- Bask_Test_3_Torch.R
+++ Bask_Test_3_Torch.R
@@ -13,6 +13,7 @@
 install.packages("torch", lib="/bask/projects/v/vjgo8416-xchen")
 .libPaths("/bask/projects/v/vjgo8416-xchen")
 library(torch)
+install_torch()
 
 
 install.packages("GPUmatrix", lib="/bask/projects/v/vjgo8416-xchen")
