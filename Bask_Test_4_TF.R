204eda8d2dc9026feec7c26baea21d9055455dd0
xc308 <xc308@exeter.ac.uk>
2024-04-02 00:05:54 +0100

    add py path

--- Bask_Test_4_TF.R
+++ Bask_Test_4_TF.R
@@ -32,17 +32,16 @@ library(Rcpp)
 
 
 # Specify the path to the Python executable
-#python_executable <- "/bask/apps/live/EL8-ice/software/Python/3.9.5-GCCcore-10.3.0/bin/python"
+# obtained by run cmd: which python
+python_executable <- "/bask/apps/live/EL8-ice/software/Python/3.9.5-GCCcore-10.3.0/bin/python"
 
 # Use the specified Python executable
-#reticulate::use_python(python_executable)
+reticulate::use_python(python_executable)
 
 
 
 #remotes::install_github("rstudio/tensorflow")
 remotes::install_github("rstudio/tensorflow", lib = "/bask/projects/v/vjgo8416-xchen/TF", force = T)
-
-
 
 .libPaths("/bask/projects/v/vjgo8416-xchen/TF")
 library(tensorflow)
