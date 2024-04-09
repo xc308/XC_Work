77e6a4d2a890767715708306a5e5999c404dcbff
xc308 <xc308@exeter.ac.uk>
2024-04-09 15:49:48 +0100

    chol_inv_gpu

---
+++ Fn_chol_inv_gpu.R
@@ -0,0 +1,43 @@
+#============
+# 9 Apr. 2024
+#============
+
+# Aim:
+  # funtion to construct inverse of a GPU matrix using cholesky
+
+# Arg:
+  # A_gpu: a matrix of GPUmatrix class, real, symmetry, p.d.
+
+
+chol_inv_gpu <- function(A_gpu) {
+  require(GPUmatrix)
+  
+  I <- diag(1, nrow(A_gpu), nrow(A_gpu))
+  A_gpu_inv <- chol_solve(t(chol(A_gpu)), I)
+  
+  return(A_gpu_inv)
+}
+
+
+#======================
+# Test on a GPU matrix
+#======================
+
+#a.gpu <- as.gpu.matrix(c(4, 1, 1, 3), 2, 2)
+#Tst_sym_pd_gpu(a.gpu)
+# [1] "Symmetric: Yes"
+# [1] "p.d.: Yes"
+
+
+#a.gpu %*% chol_inv_gpu(a.gpu)
+# GPUmatrix
+#torch_tensor
+#1.0000  0.0000
+#0.0000  1.0000
+#[ CPUDoubleType{2,2} ]
+
+
+
+
+
+
