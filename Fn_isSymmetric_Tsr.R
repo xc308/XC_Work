9271c32f4ebb20d0f436fb814460dbb444be0e5f
xc308 <xc308@exeter.ac.uk>
2024-04-14 22:19:11 +0100

    isSymmetric_Tsr

---
+++ Fn_isSymmetric_Tsr.R
@@ -0,0 +1,31 @@
+#=============
+# 14 Apr. 2024
+#=============
+# Aim:
+  # a function that can check a 2D tensor matrix's symmetricity directly
+    # on GPU device. 
+
+
+isSymmetric_Tsr <- function(Tsr_mat) {
+  
+  Tsr_dim <- torch_dim(Tsr_mat) # a vector
+  
+  if (length(Tsr_dim) != 2){
+    stop("Input tensor must be a 2D matrix.")
+  }
+  
+  Tsr_mat_t <- torch_t(Tsr_mat)
+  
+  eq_mat <- torch_eq(Tsr_mat, Tsr_mat_t)
+  
+  if (torch_prod(eq_mat) != 1) {
+    stop("Input tensor not symmetric")
+  } else {
+    print("Symmetric: Yes")
+  }
+}
+
+
+
+
+
