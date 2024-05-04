1f57bceae97239c04f667ec38adeef17ac401e29
xc308 <xc308@exeter.ac.uk>
2024-05-04 22:01:59 +0100

    8 CPUs

--- 061_2D_Inf_neg_logL_CAR_GPU_CPU.R
+++ 061_2D_Inf_neg_logL_CAR_GPU_CPU.R
@@ -333,7 +333,7 @@ lower_bound <- c(rep(NA, sum(is.na(all_pars_lst[[1]]))),  # A
 #=============================
 
 # Create a cluster
-cl <- makeCluster(10)  
+cl <- makeCluster(8)  
 
 # Set the created cluster as the default for parallel computation
 setDefaultCluster(cl = cl)  
