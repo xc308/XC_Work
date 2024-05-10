39de7dc5378767b22f615cae2ee55d6194605c18
xc308 <xc308@exeter.ac.uk>
2024-05-10 13:48:33 +0100

    4000 CPU

---
+++ Test_CPU_4000.R
@@ -0,0 +1,17 @@
+#============
+# 10 May 2024
+#============
+
+# Aim:
+  # want to know how long will it take for CPU to compute 
+  # matrix multiplication 
+
+C <- matrix(rnorm(4000*5), 20000, 20000)
+D <- matrix(rnorm(4000*5), 20000, 20000)
+
+CD <- C %*% D
+
+
+
+
+
