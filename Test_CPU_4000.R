1fde18ae0c5bb7282b9dd9c5ef1484cf3c83ce0d
xc308 <xc308@exeter.ac.uk>
2024-05-13 15:39:46 +0100

    10 times

--- Test_CPU_4000.R
+++ Test_CPU_4000.R
@@ -12,6 +12,56 @@ D <- matrix(rnorm(6600*5), 33000, 33000)
 CD <- C %*% D
 
 
+C <- matrix(rnorm(6600*5), 33000, 33000)
+D <- matrix(rnorm(6600*5), 33000, 33000)
+
+CD <- C %*% D
 
 
+C <- matrix(rnorm(6600*5), 33000, 33000)
+D <- matrix(rnorm(6600*5), 33000, 33000)
+
+CD <- C %*% D
+
+
+C <- matrix(rnorm(6600*5), 33000, 33000)
+D <- matrix(rnorm(6600*5), 33000, 33000)
+
+CD <- C %*% D
+
+
+C <- matrix(rnorm(6600*5), 33000, 33000)
+D <- matrix(rnorm(6600*5), 33000, 33000)
+
+CD <- C %*% D
+
+
+C <- matrix(rnorm(6600*5), 33000, 33000)
+D <- matrix(rnorm(6600*5), 33000, 33000)
+
+CD <- C %*% D
+
+
+C <- matrix(rnorm(6600*5), 33000, 33000)
+D <- matrix(rnorm(6600*5), 33000, 33000)
+
+CD <- C %*% D
+
+
+C <- matrix(rnorm(6600*5), 33000, 33000)
+D <- matrix(rnorm(6600*5), 33000, 33000)
+
+CD <- C %*% D
+
+
+C <- matrix(rnorm(6600*5), 33000, 33000)
+D <- matrix(rnorm(6600*5), 33000, 33000)
+
+CD <- C %*% D
+
+
+C <- matrix(rnorm(6600*5), 33000, 33000)
+D <- matrix(rnorm(6600*5), 33000, 33000)
+
+CD <- C %*% D
 
