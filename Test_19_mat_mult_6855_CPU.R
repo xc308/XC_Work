33a6fb6dc83ca10fd90da6cdaa5006c7a528a713
xc308 <xc308@exeter.ac.uk>
2024-05-19 23:34:55 +0100

    Test 19 mat mult CPU 6855

---
+++ Test_19_mat_mult_6855_CPU.R
@@ -0,0 +1,162 @@
+#============
+# 19 May 2024
+#============
+
+# Aim:
+  # mircro benchmark the matrices multiplication time on CPU
+  # with BLAS set to 36 threads
+  
+  # want to know how time scale up for pairs of matrices multipications
+  # when the matrices size
+  # grows from 3793 by 3793 to 6855 by 6855
+
+
+
+#==============
+# GPU settings
+#==============
+#------
+# torch
+#------
+# Set the library path to the desired directory
+.libPaths("/bask/projects/v/vjgo8416-xchen")
+
+# Load the torch library
+library(torch)
+
+
+#torch_set_num_interop_threads(2)
+#torch_set_num_threads(2)
+
+cat("inter threads:", "\n")
+torch_get_num_interop_threads()
+
+cat("intra threads:", "\n")
+torch_get_num_threads()
+
+
+#-------------------------------
+# check BLAS and OPENBLAS info: only 36 works
+#-------------------------------
+#install.packages("RhpcBLASctl")
+.libPaths("/bask/projects/v/vjgo8416-xchen")
+library(RhpcBLASctl)
+
+
+#cat("Check Current BLAS Library", "\n")
+#sessionInfo()
+
+cat("Check the current number of BLAS threads", "\n")
+blas_get_num_procs()
+
+#blas_set_num_threads(48)
+
+#cat("Updated BLAS threads:", "\n")
+#blas_get_num_procs()
+
+
+#-----------
+# GPUmatrix
+#------------
+
+#install.packages("GPUmatrix", lib="/bask/projects/v/vjgo8416-xchen")
+.libPaths("/bask/projects/v/vjgo8416-xchen")
+library(GPUmatrix)
+
+system("nvidia-smi")
+
+
+
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
+
+A <- matrix(rnorm(6855*6855), 6855, 6855)
+B <- matrix(rnorm(6855*6855), 6855, 6855)
+A %*% B
+
